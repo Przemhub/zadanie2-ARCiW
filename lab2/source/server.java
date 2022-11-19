e9dacdf00a5465519deef06204a75bec111ac452
Przemhub <244010@edu.p.lodz.pl>
2022-11-19 10:46:03 +0100

    first commit

---
+++ lab2/source/server.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.time.*;
+import java.util.Locale;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class Test {
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(4080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+        Instant inst = Instant.now();
+        ZonedDateTime plTime = inst.atZone(ZoneId.of("Europe/Warsaw"));
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+            String response = "Hello World from java!\n"+sdf.format(plTime);
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+	    System.out.println("Served hello world...");
+        }
+    }
+
+}
